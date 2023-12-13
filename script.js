32a3980faadd3d958522528ad424e262035b80af
Jeylson0 <jhonalson.lugada@gmail.com>
2023-12-13 22:12:04 +0800

    Version 2.0.0 Tic Tac Toe project
    Updated UI, created several modules which contains the gamesettings,
    DOM, and the gameboard and a factory function for the player object.
    Each module uses properties and methods from one another while avoiding
    polluting the global namespace.

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 //IIFE
+let DOM = (function(){
+  let cellButton = document.querySelectorAll(".cell-btn");
+  let cellInput = document.querySelectorAll("input[type='hidden']");
+  let imgPlayerContainer = document.querySelectorAll("img[alt='playerSign']");
+  let playerNotif = document.querySelector(".player-notification");
+  const playButton = document.querySelector(".UI-button");
+  const dbCross50 = "assets/imgs/db-cross-50.svg";
+  const dbCircle50 = "assets/imgs/db-circle-50.svg";
+  const dbCross = "assets/imgs/db-cross.svg";
+  const dbCircle = "assets/imgs/db-circle.svg";
+  const dbCrossWin = "assets/imgs/db-cross-win.svg";
+  const dbCircleWin = "assets/imgs/db-circle-win.svg";
+
+
+  return {
+    cellButton,
+    cellInput, 
+    dbCross50, 
+    dbCircle50, 
+    dbCross, 
+    dbCircle, 
+    dbCrossWin, 
+    dbCircleWin, 
+    imgPlayerContainer,
+    playButton,
+    playerNotif
+  }
+})();
+
+
 const gameBoard = (function(){
 
-  let grid = ['1', '2',  '3', '4',  '5', '6', '7', '8', '9' ]
-
+  let grid = ['1', '2',  '3', '4',  '5', '6', '7', '8', '9' ];
+  let gameStatus = true;
   let winCode = [
     ['1', '2', '3'],
     ['4', '5', '6'],
@@ -12,7 +42,7 @@ const gameBoard = (function(){
     ['3', '6', '9'],
     ['3', '5', '7'],
     ['1', '5', '9']
-  ]
+  ];
 
   let boardUI = 
   `
@@ -20,178 +50,235 @@ const gameBoard = (function(){
   ${grid[3]} | ${grid[4]} | ${grid[5]} 
   ${grid[6]} | ${grid[7]} | ${grid[8]} 
   `
+  let rounds = 0;
 
- let playerOne = player('player one', 'x')
- let playerTwo = player('player two', 'o')
+ let playerOne = player('Player X', 'x', DOM.dbCross50, DOM.dbCross, DOM.dbCrossWin)
+ let playerTwo = player('Player O', 'o', DOM.dbCircle50, DOM.dbCircle, DOM.dbCircleWin)
  
-  function play() {
-    let rounds = 9
-    let gameEnd;
-
-    function result(){
-      if (grid.length == 0) {
-        console.log('No wins. Try again')
-        return gameEnd = true
-      }
-  
-      if (playerOne.playerWin == true) {
-        console.log('player one: win')
-        return gameEnd = true
-      }
-  
-      if (playerTwo.playerWin == true) {
-        console.log('player two: win')
-        return gameEnd = true
-      }
+ 
+  function result(){
+    ++rounds
+    if (playerOne.playerWin == true) {
+      DOM.playerNotif.textContent = `${playerOne.playerName} Win!`
+      DOM.playButton.setAttribute('value', 'Try Again')
+      DOM.playButton.classList.remove('disabled')
+      DOM.playButton.removeEventListener('click', gameSettings.startGame)
+      DOM.playButton.addEventListener('click', gameSettings.resetGame)
+      return rounds = 0;
     }
 
-    for (let i = 0; i < rounds; i++) {
+    if (playerTwo.playerWin == true) {
+      DOM.playerNotif.textContent = `${playerTwo.playerName} Win!`
+      DOM.playButton.setAttribute('value', 'Try Again')
+      DOM.playButton.classList.remove('disabled')
+      DOM.playButton.removeEventListener('click', gameSettings.startGame)
+      DOM.playButton.addEventListener('click', gameSettings.resetGame)
+      return rounds = 0;
+    }
 
-      playerOne.getCell()
-      evaluateWin(playerOne)
-      result()
-
-      if(gameEnd == true) {
-        break
-      }
-
-      playerTwo.getCell()
-      evaluateWin(playerTwo)
-      result()
-
-      if(gameEnd == true) {
-        break
-      }
-
+    if (rounds == 9) {
+      DOM.playerNotif.textContent = 'No wins. Try again'
+      DOM.playButton.setAttribute('value', 'Try Again')
+      DOM.playButton.classList.remove('disabled')
+      DOM.playButton.removeEventListener('click', gameSettings.startGame)
+      DOM.playButton.addEventListener('click', gameSettings.resetGame)
+      return rounds = 0;
     }
 
   }
 
-  function evaluateWin(player) {
-
-    let playerArray = player.playerChoice
-    console.log(playerArray)
-    let counter = 0;
-
-     for (let i = 0; i < winCode.length; i++) {
-      let winArray = winCode[i];
-      let filtered = playerArray.filter((value) => winArray.includes(value))
-      
-      if (winArray.length === filtered.length) {
-        for (let j = 0; j < winArray.length; j++) {
-          let winValue = winArray[j];
-          let checkValue = filtered.some((value) => value == winValue)
-          if(checkValue) {
-            counter++
-          }
-        }
-      }
-
-      if (counter == 3) {
-        return player.playerWin = true
-      }
-
-    }
-
-  }
-
-  return {play, evaluateWin, grid, boardUI, playerOne, playerTwo}
+  return {result, grid, boardUI, playerOne, playerTwo, winCode, gameStatus}
 })();
 
-
-//Player factory function
-function player(name, sign) {
-  let playerName = name;
-  let playerSign = sign;
-  let playerChoice = [];
-
-  //function ask for users input, input passed to findMatch()
-  function getCell() {
-    let cell = prompt(
-    `
-    ${playerName}
-    ${gameBoard.boardUI}
-    Place your input:
-     `
-     )
-
-    findMatch(cell);
-  }
-
-  //function takes 'cell' private variable. finds 'input' in gameBoard.grid which returns the first value that passes the cb function
-  function findMatch(cell) {
-    let match = gameBoard.grid.find(element => element == cell)
-    evaluate(match)
-  }
-
-  //function takes the 'matched' value from the grid.
-  function evaluate(match) {
-    
-    //if true, that value is pushed to playerChoice array, get the index of that value and use that index to change gameBoard.boardUI and splice() it from grid array
-    if (match) {
-      playerChoice.push(match)
-      let index = gameBoard.grid.indexOf(match)
-      gameBoard.boardUI = gameBoard.boardUI.replace(gameBoard.grid[index], `${playerSign}`)
-      gameBoard.grid.splice(index, 1)
-      console.log(gameBoard.boardUI)
-    }
-
-    //if false, initialize another input to 'cell' and loop to findMatch()
-    if (!match) {
-      alert('invalid')
-
-      cell = prompt(
-        `
-        ${playerName}
-        ${gameBoard.boardUI}
-        Place your input:
-         `
-         )
-      findMatch(cell)
-    }
-  }
-  return {getCell, playerChoice , playerSign, playerName}
-
-}
 
 const gameSettings = (function(){
 
   function resetGame() {
-    gameBoard.grid = ['1', '2',  '3', '4',  '5', '6', '7', '8', '9' ]
+    gameBoard.grid = ['1', '2',  '3', '4',  '5', '6', '7', '8', '9' ];
 
-    gameBoard.playerOne.playerChoice.length = 0
-    gameBoard.playerTwo.playerChoice.length = 0
-    gameBoard.playerOne.playerSign = undefined
-    gameBoard.playerTwo.playerSign = undefined
-    gameBoard.playerOne.playerWin = undefined
-    gameBoard.playerTwo.playerWin = undefined
+    gameBoard.playerOne.playerChoice.length = 0;
+    gameBoard.playerTwo.playerChoice.length = 0;
+    gameBoard.gameStatus = true;
+  
+
+    if(gameBoard.playerOne.playerWin) {
+      gameBoard.playerTwo.getCell()
+    }
+    
+    if(gameBoard.playerTwo.playerWin) {
+      gameBoard.playerOne.getCell()
+    }
+
+    gameBoard.playerOne.playerWin = false
+    gameBoard.playerTwo.playerWin = false
 
     gameBoard.boardUI =   `
     ${gameBoard.grid[0]} | ${gameBoard.grid[1]} | ${gameBoard.grid[2]} 
     ${gameBoard.grid[3]} | ${gameBoard.grid[4]} | ${gameBoard.grid[5]} 
     ${gameBoard.grid[6]} | ${gameBoard.grid[7]} | ${gameBoard.grid[8]} 
     `
+
+    DOM.imgPlayerContainer.forEach((element) => {
+      element.setAttribute('src', '')
+      element.className = 'unassigned-cell'
+    })
+
+    DOM.playButton.classList.add('disabled')
+
   }
 
-  return {resetGame}
+
+  function switchPlayer(sign) {
+    //switch to playertwo
+    if(sign == 'x') {
+      gameBoard.playerTwo.getCell()
+    }
+
+    //switch to playerOne
+    if(sign == 'o') {
+      gameBoard.playerOne.getCell()
+    }
+  }
+
+  function startGame(){
+    gameBoard.playerOne.getCell()
+    DOM.playButton.classList.add('disabled')
+  }
+
+  DOM.playButton.addEventListener('click', startGame)
+
+  return {resetGame, switchPlayer, startGame}
 })();
 
 
-const DOM = (function(){
 
-  let cellButton = document.querySelectorAll(".cell-btn")
-  let cellInput = document.querySelectorAll("input[type='hidden']")
-  let input;
+//Player factory function
+function player(name, sign, hover, playerImg, playerImgWin) {
+  let playerName = name;
+  let playerSign = sign;
+  let hoverUI = hover;
+  let imgSign = playerImg
+  let imgSignWin = playerImgWin
+  let playerChoice = [];
+  let playerWin;
+  let playerObj; //reference to specific player obj
 
-  cellButton.forEach((element) => {
-    element.addEventListener('click', (event) => {
-      return input = element.firstChild.value
-    })
-  })
+  function getCell() {
+    DOM.playerNotif.textContent = `${playerName} Turn`
+    if(gameBoard.gameStatus == false){
+      return console.log('game ended')
+    }
+
+    if(gameBoard.gameStatus == true) {
+      DOM.cellButton.forEach(element => {
+        playerObj = this;
+        element.addEventListener('click', getDOMInput)
+        element.addEventListener('mouseover', hoverCell)
+        element.addEventListener('mouseout', hoverOutCell)
+      });
+    }
+    
+  }
+
+  function hoverOutCell() {
+    let imgContainer = this.children[1];
+    if (imgContainer.className == 'assigned-cell') {
+      return 
+    } else {
+      imgContainer.setAttribute('src', '')
+    }
+
+  }
+
+  function hoverCell(){
+
+    let imgContainer = this.children[1];
+    if (imgContainer.className == 'assigned-cell')  {
+      return 
+    } else {
+      imgContainer.setAttribute('src', hoverUI)
+    }
+
+  }
+
+  function getDOMInput() {
+    let cell = this.firstElementChild.value;
+    findMatch(cell); 
+  }
+
+  function findMatch(cell) {
+    let match = gameBoard.grid.find(element => element == cell)
+    evaluate(match)
+  }
+
+  function evaluate(match) {
+    
+    if (match) {
+      playerObj.playerChoice.push(match)
+      let index = gameBoard.grid.indexOf(match)
+      let DOMIndex = match - 1
+      gameBoard.boardUI = gameBoard.boardUI.replace(gameBoard.grid[index], `${playerSign}`)
+      gameBoard.grid.splice(index, 1)
+
+      DOM.imgPlayerContainer[DOMIndex].setAttribute('src', imgSign)
+      DOM.imgPlayerContainer[DOMIndex].className = 'assigned-cell'
+        
+      evaluateWin()
+      gameBoard.result()
+    
+      DOM.cellButton.forEach(element => {
+        element.removeEventListener('click', getDOMInput)
+        element.removeEventListener('mouseover', hoverCell)
+        element.removeEventListener('mouseout', hoverOutCell)
+      });
+
+      if (playerObj.playerWin == false || playerObj.playerWin == undefined) {
+        gameSettings.switchPlayer(playerObj.playerSign)
+      } 
+      
+    }
+
+    if (!match) {
+      alert('Place has been taken')
+      return
+    }
+
+  }
+
+  function evaluateWin() {
+    let playerArray = playerObj.playerChoice
+    let counter = 0;
+
+    for (let i = 0; i < gameBoard.winCode.length; i++) {
+      let winArray = gameBoard.winCode[i];
+      let filtered = playerArray.filter((value) => winArray.includes(value))
+      
+      if (winArray.length == filtered.length) {
+        for (let j = 0; j < winArray.length; j++) {
+          let winValue = winArray[j];
+          let checkValue = filtered.some((value) => value == winValue)
+          DOM.imgPlayerContainer[winValue-1].setAttribute('src', imgSignWin)
+          if(checkValue) {
+            counter++
+          }
+        }
+      }
+
+    
+
+    }
+
+    if (counter >= 3) {
+      playerObj.playerWin = true
+    }
+
+  }
 
 
+  return {getCell, playerChoice , playerSign, playerName, playerWin}
 
-  return {cellButton, cellInput}
-})();
+}
+
+
 
 
